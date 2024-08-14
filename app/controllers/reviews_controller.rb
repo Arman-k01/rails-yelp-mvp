c8a2030a55cf7c61d0650aa2799047768f41bb83
Arman Kurtovic <armankurtovic01@gmail.com>
2024-08-14 12:50:59 +0200

    Rails Yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      if Rails.env.test?
+        render :new
+      else
+        render 'restaurants/show'
+      end
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
